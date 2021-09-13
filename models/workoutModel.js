ea7bbf9ea8534199d2bd481d95ddaaf0463301ca
Adrian B. Cronin <cronin.adrianb@gmail.com>
2021-09-12 18:52:23 -0700

    created the schema for the workoutModel

---
+++ models/workoutModel.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+    date: {
+        type: Date,
+        default: Date.now
+    },
+    exercises: [
+        {
+            type: {
+                type: String,
+                trim: true,
+                required: true
+            },
+            name: {
+                type: String,
+                trim: true,
+                required: true
+            },
+            duration: {
+                type: Number,
+                required: true
+            },
+            weight: {
+                type: Number,
+                required: false
+            },
+            reps: {
+                type: Number,
+                require: false
+            },
+            sets: {
+                type: Number,
+                require: false
+            },
+            distance: {
+                type: Number,
+                require: false
+            },
+        }
+    ]
+});
