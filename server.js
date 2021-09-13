2997045d4cb3e88b9ee7a9827679ed8b7fabf29f
Adrian B. Cronin <cronin.adrianb@gmail.com>
2021-09-12 19:30:39 -0700

    added router exports to all the controller files

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require("express");
+const logger = require("morgan");
 const mongoose = require("mongoose");
+
+const routes = require("./controllers");
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -16,7 +20,7 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
 });
 
 // routes
-app.use(require("./controllers"));
+app.use(routes);
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
