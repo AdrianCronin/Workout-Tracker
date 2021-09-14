e37b3e3f5a52e436bfe012d6dab93f81dfa57e33
Adrian B. Cronin <cronin.adrianb@gmail.com>
2021-09-13 18:26:41 -0700

    added the /api/workouts route that returns all the workouts in the database

--- controllers/api/workoutRoutes.js
+++ controllers/api/workoutRoutes.js
@@ -1,7 +1,14 @@
 const router = require('express').Router();
 
-const Workout = require('../../models');
+const db = require('../../models');
 
-
+router.get("/", async (req, res) => {
+    try {
+        const workoutData = await db.Workout.find({});
+        res.status(200).json(workoutData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 module.exports = router;
