57cfb439bc1824ac12e8a311435f25cc272cdd16
Adrian B. Cronin <cronin.adrianb@gmail.com>
2021-09-14 20:05:17 -0700

    started a route to add exercises to a work out

--- controllers/api/workoutRoutes.js
+++ controllers/api/workoutRoutes.js
@@ -2,10 +2,23 @@ const router = require('express').Router();
 
 const db = require('../../models');
 
+// get all workouts
 router.get("/", async (req, res) => {
     try {
         const workoutData = await db.Workout.find({});
         res.status(200).json(workoutData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+
+
+
+// add exercises to workout route
+router.put("/:id", async (req,res) => {
+    try {
+        res.status(200).json(req.body);
     } catch (err) {
         res.status(500).json(err);
     }
