48187e092b2d777c3dff8b9e1e3ea4c52dc52564
Adrian B. Cronin <cronin.adrianb@gmail.com>
2021-09-14 20:39:29 -0700

    created a create new workout post route

--- controllers/api/workoutRoutes.js
+++ controllers/api/workoutRoutes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 
+const { Workout } = require('../../models');
 const db = require('../../models');
 
 // get all workouts
@@ -12,7 +13,17 @@ router.get("/", async (req, res) => {
     }
 });
 
+// create new workout route
+router.post("/", async (req, res) => {
+    try {
 
+        const workoutData = await db.Workout.create(req.body);
+        res.status(200).json(workoutData)
+        
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 
 // add exercises to workout route
